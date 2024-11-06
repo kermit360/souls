95f7ea2a8e29d08c30418d63ed8edf92ba30bc10
Josué ACG <Jos@MacBook-Pro-de-Mc.local>
2024-11-06 16:47:15 -0600

    avance_bruja

---
+++ Bruja/colisión.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@onready var anim_tree: AnimationTree = $bruja/AnimationTree
+
+const SPEED = 100.0
+var personaje = null
+var distancia = 0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else :
+		velocity = position.direction_to(personaje.position)*SPEED
+	if position.distance_to(personaje.position)< 200:
+		anim_tree["parameters/walk/blend_amount"]= 1
+		move_and_slide()
+	
+func _ready() -> void:
+	personaje=  get_parent().get_node("main_char")
+	
+#func _process(delta: float) -> void:
+#	if 
+
+func _on_area_entered(area):
+	if area.is_in_group("main"):
+		anim_tree ["parameters/Blend2/blend_amount"]= 1
